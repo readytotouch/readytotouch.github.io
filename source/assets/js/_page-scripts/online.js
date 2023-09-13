2f5ea3738c9421210b4d0b56d44adab9d833c38f
Victor Poprozhuk <victor.poprozhuk@gmail.com>
2023-09-14 00:03:15 +0300

    13.09.2023 add online page (#2)

---
+++ source/assets/js/_page-scripts/online.js
@@ -0,0 +1,148 @@
+// library settings ApexCharts
+
+const date = new Date()
+const month = date.getMonth() + 1
+const daysInMonth = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+
+function getMonth() {
+    return month < 10 ? ".0" + month : month;
+}
+
+const correctMonth = getMonth();
+
+function createData() {
+    const result = []
+    for (let i = 1; i < daysInMonth; i++) {
+        result.push({
+            x: Math.floor(Math.random() * 100),
+            y: i >= 10 || i === 0 ? i + correctMonth : "0" + i + correctMonth
+        });
+    }
+    return result
+}
+
+const data = createData()
+
+const options = {
+    series: [{
+        name: "Views",
+        data: data.map(item => item.x),
+    }],
+    chart: {
+        type: "area",
+        height: 340,
+        zoom: {
+            enabled: false,
+        },
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: ["#8D6DFF"],
+    fill: {
+        type: "gradient",
+        colors: ['#8D6DFF'],
+        gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.65,
+            opacityTo: 0.3,
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        curve: "smooth",
+    },
+    labels: data.map(item => item.y),
+    xaxis: {
+        type: "datey",
+    },
+    yaxis: {
+        labels: {
+            style: {
+                colors: ["#575D65"],
+                fontSize: "14px",
+            },
+        },
+    },
+    markers: {
+        size: 5,
+        colors: ["#fff"],
+        strokeColors: "#8D6DFF",
+        strokeWidth: 2,
+        strokeOpacity: 0.9,
+        strokeDasharray: 1,
+        fillOpacity: 1,
+        shape: "circle",
+        radius: 2,
+    },
+};
+
+const registration = {
+    series: [{
+        name: "Views",
+        data: data.map(item => item.x),
+    }],
+    chart: {
+        type: "area",
+        height: 340,
+        zoom: {
+            enabled: false,
+        },
+        toolbar: {
+            show: false,
+        },
+    },
+    colors: ["#5484FF"],
+    fill: {
+        type: "gradient",
+        colors: ['#5484FF'],
+        gradient: {
+            shadeIntensity: 1,
+            opacityFrom: 0.65,
+            opacityTo: 0.3,
+        },
+    },
+    dataLabels: {
+        enabled: false,
+    },
+    stroke: {
+        curve: "smooth",
+    },
+    labels: data.map(item => item.y),
+    xaxis: {
+        type: "datey",
+    },
+    yaxis: {
+        labels: {
+            style: {
+                colors: ["#575D65"],
+                fontSize: "14px",
+            },
+        },
+    },
+    markers: {
+        size: 5,
+        colors: ["#fff"],
+        strokeColors: "#5484FF",
+        strokeWidth: 2,
+        strokeOpacity: 0.9,
+        strokeDasharray: 1,
+        fillOpacity: 1,
+        shape: "circle",
+        radius: 2,
+    },
+};
+
+const chartArea = document.querySelector(".js-chart-online-statistics")
+if (chartArea) {
+    const chartApex = new ApexCharts(chartArea, options);
+    chartApex.render();
+}
+
+const chartRows = document.querySelector(".js-chart-registration-statistics")
+if (chartRows) {
+    const chartRowsApex = new ApexCharts(chartRows, registration);
+    chartRowsApex.render();
+}
---
+++ source/gulp-tasks/injectString.js
@@ -0,0 +1,42 @@
+import { src } from "gulp";
+import pump from "pump";
+import inject from "gulp-inject";
+import fs from "fs";
+
+function setStringSrc(typeFIle, dataSrc) {
+	return typeFIle === ".css"
+		? `<link href="${dataSrc}" rel="stylesheet" />`
+		: `<script src="/${dataSrc}"></script>`;
+}
+
+export function injectString(cb, pathInput, pathOutput, build) {
+	return pump(
+		[
+			src(pathInput),
+			inject(src([build], { read: false }), {
+				transform: (filepath) => {
+					const fileExtension = filepath.slice(
+						filepath.indexOf("assets"),
+					);
+					const typeFile = filepath.slice(filepath.lastIndexOf("."));
+					let stringToInsert = filepath.slice(
+						filepath.lastIndexOf("/") + 1,
+						filepath.lastIndexOf("."),
+					);
+					if (stringToInsert.lastIndexOf("_") >= 0) {
+						stringToInsert = stringToInsert.slice(
+							0,
+							stringToInsert.lastIndexOf("_"),
+						);
+					}
+					fs.mkdirSync(pathOutput, { recursive: true });
+					fs.writeFileSync(
+						`${pathOutput}${stringToInsert}.html`,
+						setStringSrc(typeFile, fileExtension),
+					);
+				},
+			}),
+		],
+		cb,
+	);
+}
