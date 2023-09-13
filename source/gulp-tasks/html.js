e8955389981d01092877d59fa5d124b80c71d4fe
VictorPoprozhuk <victor.poprozhuk@gmail.com>
2023-09-13 23:22:10 +0300

    13.09.2023 add online page

---
+++ source/gulp-tasks/html.js
@@ -0,0 +1,26 @@
+import { src, dest } from "gulp";
+import pump from "pump";
+import plumber from "gulp-plumber";
+import fileinclude from "gulp-file-include";
+import notify from "gulp-notify";
+
+// assemble html parts
+export default function html(cb, inputHtml, basepath, outputHtml) {
+	return pump(
+		[
+			src(inputHtml),
+			plumber({
+				errorHandler: notify.onError("Error: <%= error.message %>"),
+			}),
+			fileinclude({
+				prefix: "@@",
+				basepath: basepath,
+				context: {
+
+				},
+			}),
+			dest(outputHtml),
+		],
+		cb,
+	);
+}
