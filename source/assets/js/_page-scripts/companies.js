ece3bebea4a6069d4d4185c499a3339ed1e836bb
VictorPoprozhuk <victor.poprozhuk@gmail.com>
2024-04-11 21:59:57 +0300

    add empty block, update styles, refactoring 11.04

--- source/assets/js/_page-scripts/companies.js
+++ source/assets/js/_page-scripts/companies.js
@@ -3,3 +3,48 @@ document.querySelectorAll(".card__action-button").forEach((button) => {
 		event.target.classList.toggle("in-favorite");
 	});
 });
+
+const showListButton = document.querySelector("#show_list");
+const showEmptySearchButton = document.querySelector("#show_empty_search");
+const showEmptyCompaniesListButton = document.querySelector(
+	"#show_empty_companies",
+);
+
+function listener(selector, callback) {
+	selector.addEventListener("click", (event) => callback(event));
+}
+
+function showBlock(selectors) {
+	selectors.forEach((selector) => {
+		document.getElementById(selector).classList.remove("d-none");
+	});
+}
+
+function hideBlock(selectors) {
+	selectors.forEach((selector) => {
+		document.getElementById(selector).classList.add("d-none");
+	});
+}
+
+listener(showListButton, () => {
+	hideBlock(["search_result_empty_block"]);
+	showBlock(["search_result_list", "search_result_filter_used"]);
+});
+
+listener(showEmptyCompaniesListButton, () => {
+	hideBlock([
+		"search_result_list",
+		"search_result_filter_used",
+		"search_result_no_result",
+	]);
+	showBlock(["search_result_empty_block", "search_result_no_companies"]);
+});
+
+listener(showEmptySearchButton, () => {
+	hideBlock(["search_result_list", "search_result_no_companies"]);
+	showBlock([
+		"search_result_empty_block",
+		"search_result_no_result",
+		"search_result_filter_used",
+	]);
+});
