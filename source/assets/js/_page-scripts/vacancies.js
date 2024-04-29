69a1fd41fac3ce6fa1d54bc364a557485c3cc17c
Victor Poprozhuk <victor.poprozhuk@gmail.com>
2024-04-29 21:46:01 +0300

    Companies, projects and vacancies (#8)

---
+++ source/assets/js/_page-scripts/vacancies.js
@@ -0,0 +1,83 @@
+function toggleCheckbox(target, child) {
+	target.forEach((element) => {
+		element.addEventListener("click", (event) => {
+			let target = event.target;
+
+			if (
+				!target.classList.contains("is-checked") &&
+				target !== element.querySelector(child)
+			) {
+				target.classList.add("is-checked");
+			} else if (target === element.querySelector(child)) {
+				target.parentElement.classList.remove("is-checked");
+			} else {
+				target.classList.remove("is-checked");
+			}
+		});
+	});
+}
+
+const checkboxFilled = document.querySelectorAll(".js-checkbox-filled");
+const checkboxFilledElementClassName = ".js-checkbox-filled-element";
+
+if (checkboxFilled.length > 0) {
+	toggleCheckbox(checkboxFilled, checkboxFilledElementClassName);
+}
+
+document.querySelectorAll(".card__action-button").forEach((button) => {
+	button.addEventListener("click", (event) => {
+		event.target.classList.toggle("in-favorite");
+	});
+});
+
+const $hideVacancyButtons = document.querySelectorAll(".js-hide-vacancy");
+$hideVacancyButtons.forEach(($button) => {
+	$button.addEventListener("click", (e) => {
+		const $card = e.target.closest(".js-card");
+
+		$card.classList.add("hidden");
+	});
+});
+
+const $undoHideVacancyButtons = document.querySelectorAll(
+	".js-undo-hide-vacancy",
+);
+$undoHideVacancyButtons.forEach(($button) => {
+	$button.addEventListener("click", (e) => {
+		const $card = e.target.closest(".js-card");
+		$card.classList.remove("hidden");
+		$card
+			.querySelector(".js-hide-company-vacancies-block")
+			.classList.remove("hidden", "hidden-all");
+	});
+});
+
+const $hideAdditionalBlockButtons = document.querySelectorAll(
+	".js-hide-block-button",
+);
+$hideAdditionalBlockButtons.forEach(($button) => {
+	$button.addEventListener("click", (e) => {
+		const $block = e.target.closest(".js-hide-company-vacancies-block");
+		$block.classList.add("hidden");
+	});
+});
+
+const $hideAllVacancyCompanyButton = document.querySelectorAll(
+	".js-hide-all-vacancy-button",
+);
+$hideAllVacancyCompanyButton.forEach(($button) => {
+	$button.addEventListener("click", (e) => {
+		const $block = e.target.closest(".js-hide-company-vacancies-block");
+		$block.classList.add("hidden-all");
+	});
+});
+
+const $undoHideCompanyVacanciesButton = document.querySelectorAll(
+	".js-undo-hide-company-vacancies",
+);
+$undoHideCompanyVacanciesButton.forEach(($button) => {
+	$button.addEventListener("click", (e) => {
+		const $block = e.target.closest(".js-hide-company-vacancies-block");
+		$block.classList.remove("hidden-all");
+	});
+});
