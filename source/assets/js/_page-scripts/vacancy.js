14aa519c725369366ae8507382c887da731b6d29
VictorPoprozhuk <victor.poprozhuk@gmail.com>
2024-05-21 22:48:17 +0300

    refactor vacancy page, add some page, add old vacancy modals, projects, update styles, refactoring 21.05

---
+++ source/assets/js/_page-scripts/vacancy.js
@@ -0,0 +1,14 @@
+import favorite from "../common/favorite";
+import feedback from "../common/feedback";
+
+favorite();
+
+const feedbackButton = document.querySelectorAll(".js-feedback-button");
+const radioButton = document.querySelectorAll(".js-profile-radio");
+const sendButton = document.querySelector(".js-send-profile");
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	if (feedbackButton !== null) {
+// 		feedback(feedbackButton, radioButton, sendButton);
+// 	}
+// });
