b466968e7ab20cd7b49a78170cada21cac5fd6ea
Yevhen Kholevchuk <holevchuk.evgeny+main@gmail.com>
2024-08-24 03:57:34 +0300

    test new commit with pre-commit hook6

---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'source/**/*.js': ['eslint --fix', 'prettier --write --ignore-unknown'],
+  'source/**/*.scss': ['stylelint --fix', 'git add'],
+}
--- source/.lintstagedrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  '*.js': ['eslint --fix', 'prettier --write --ignore-unknown'],
-  '*.scss': ['stylelint --fix', 'git add'],
-}
